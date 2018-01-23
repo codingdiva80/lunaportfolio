eeafdc9f0fc954aa645fe656ab194a8879f7b553
Luna <altynaylinstrum@yahoo.com>
2018-01-23 14:00:29 -0800

    fixed link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-function initSlider(){
-	const elements = document.querySelectorAll(".portfolio-info-div");
-	for (let i = 0; i < elements.length; i++) {
-		let element = elements[i];
-		element.parentNode.onmouseover = function(){
-			element.classList.remove("noslide");
-			element.classList.remove("slidedown");
-			element.classList.add("slideup");
-		};
-		element.parentNode.onmouseout = function(){
-			element.classList.remove("slideup");
-			element.classList.add("slidedown");
-			element.classList.add("noslide");
-		}
-	}
-}
-
 function initLinks(){
 	let divEls = document.querySelectorAll("[data-href]");
 	divEls.forEach(div => {
